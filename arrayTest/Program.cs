bfad6e74a8a0bf87c76b9a146fdbe801b68c9f36
leastXpctdMost <33549340+leastXpctdMost@users.noreply.github.com>
2019-07-27 14:00:51 +0100

    Initial commit

---
+++ arrayTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayTest
+{
+
+    public class Palindrome
+    {
+        public static bool IsPalindrome(string word)
+        {
+            string lower = word.ToLower();
+            char[] charArray = lower.ToCharArray();
+            Array.Reverse(charArray);
+            string newString = new String(charArray);
+            bool isPalin = false;
+            if (lower == newString)
+            {
+                isPalin = true;
+            }
+            return isPalin;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("Hannah"));
+            Console.ReadKey();
+
+        }
+    }
+}
